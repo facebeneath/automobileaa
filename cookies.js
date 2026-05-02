c637632a331fe59b8cc6969133da488ac951b1b2
Aldin Delic <facebeneath@aldindelic.de>
2026-05-02 19:27:21 +0200

    Ad hoc commit 2026-05-02 19:27:21

--- cookies.js
+++ cookies.js
@@ -90,14 +90,14 @@
     wrap.id = "cc-wrap";
 
     wrap.innerHTML = `
-      <div id="cc-box">
-        <h2>Cookies</h2>
-        <p>Wir verwenden Cookies für Analysen und zur Verbesserung der Website.</p>
+  <div id="cc-box">
+    <h2>Cookies</h2>
+    <p>Wir verwenden Cookies für Analysen und zur Verbesserung der Website.</p>
 
-        <button class="cc-btn cc-accept" id="cc-accept">Alle akzeptieren</button>
-        <button class="cc-btn cc-decline" id="cc-decline">Ablehnen</button>
-      </div>
-    `;
+    <button class="cc-btn cc-accept" id="cc-accept">Alle akzeptieren</button>
+    <button class="cc-btn cc-decline" id="cc-decline">Ablehnen</button>
+  </div>
+`;
 
     return wrap;
   }
